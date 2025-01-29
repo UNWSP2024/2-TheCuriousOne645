33b9d6c6777402a6e359e31b9033a7334be3f0b8
TheCuriousOne645 <tobinaustin78@gmail.com>
2025-01-29 08:27:48 -0600

    program2averageage.py

--- program2averageage.py
+++ program2averageage.py
@@ -4,11 +4,11 @@
 # Title: Average Age Calculator
 
 # Input ages of five friends
-age1 = float(input("Enter the age of friend 1: "))
-age2 = float(input("Enter the age of friend 2: "))
-age3 = float(input("Enter the age of friend 3: "))
-age4 = float(input("Enter the age of friend 4: "))
-age5 = float(input("Enter the age of friend 5: "))
+age1 = float(input("Enter the age of friend 1: 55"))
+age2 = float(input("Enter the age of friend 2: 20"))
+age3 = float(input("Enter the age of friend 3: 18"))
+age4 = float(input("Enter the age of friend 4: 19"))
+age5 = float(input("Enter the age of friend 5: 13"))
 
 # Calculate average age
 average_age = (age1 + age2 + age3 + age4 + age5) / 5
