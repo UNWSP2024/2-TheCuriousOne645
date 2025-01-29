3a7e61bb1f91190865f8e8fd075f9843ff312091
TheCuriousOne645 <tobinaustin78@gmail.com>
2025-01-29 08:32:12 -0600

    program3totalpurchase.py

--- program3totalpurchase.py
+++ program3totalpurchase.py
@@ -4,11 +4,11 @@
 # Title: Total Purchase Calculator
 
 # Input prices of five items
-item1 = float(input("Enter the price of item 1: "))
-item2 = float(input("Enter the price of item 2: "))
-item3 = float(input("Enter the price of item 3: "))
-item4 = float(input("Enter the price of item 4: "))
-item5 = float(input("Enter the price of item 5: "))
+item1 = float(input("Enter the price of item 1: $2.50"))
+item2 = float(input("Enter the price of item 2: $4.70"))
+item3 = float(input("Enter the price of item 3: $33.80"))
+item4 = float(input("Enter the price of item 4: $55.80"))
+item5 = float(input("Enter the price of item 5: $2.99"))
 
 # Calculate subtotal
 subtotal = item1 + item2 + item3 + item4 + item5
