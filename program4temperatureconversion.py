c50868cfa3f94f72edbda034a6033ac4ce8948f0
TheCuriousOne645 <tobinaustin78@gmail.com>
2025-01-29 09:14:57 -0600

    program4temperatureconversion.py

--- program4temperatureconversion.py
+++ program4temperatureconversion.py
@@ -1,10 +1,10 @@
 # Program #4: Temp Conversion
-# Author: [Your Name]
-# Date: [Today's Date]
+# Author: [Austin]
+# Date: [1/29/2025]
 # Title: Celsius to Fahrenheit Converter
 
 # Input temperature in Celsius
-celsius = float(input("Enter the temperature in Celsius: "))
+celsius = float(input("Enter the temperature in Celsius: 33"))
 
 # Convert to Fahrenheit
 fahrenheit = (9/5) * celsius + 32
