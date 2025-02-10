92a38e02c11577b9bf8d5dd45323e4d39c970248
TheCuriousOne645 <tobinaustin78@gmail.com>
2025-02-10 13:48:45 -0600

    program3averagerainfall.py

--- program3averagerainfall.py
+++ program3averagerainfall.py
@@ -1,16 +1,16 @@
-years = int(input("Enter the number of years: "))
+years = int(input("5: "))
 total_rainfall = 0
 total_months = 0
 
 for year in range(1, years + 1):
     print(f"Year {year}:")
     for month in range(1, 13):
-        rainfall = float(input(f"Enter the rainfall for month {month}: "))
+        rainfall = float(input(f"4 {month}: "))
         total_rainfall += rainfall
         total_months += 1
 
 average_rainfall = total_rainfall / total_months
 
-print(f"Number of months: {total_months}")
-print(f"Total inches of rainfall: {total_rainfall}")
-print(f"Average rainfall per month: {average_rainfall}")
+print(f"60: {total_months}")
+print(f"180: {total_rainfall}")
+print(f"3: {average_rainfall}")
