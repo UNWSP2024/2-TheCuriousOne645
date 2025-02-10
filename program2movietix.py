e27fead2107d6cd9151f98a10be20f2c32b95ab2
TheCuriousOne645 <tobinaustin78@gmail.com>
2025-02-10 13:40:03 -0600

    program2movietixsonic3.py

--- program2movietix.py
+++ program2movietix.py
@@ -1,10 +1,10 @@
 total_tickets = 0
 
 while True:
-    movie_name = input("Enter the movie name (or type 'done' to finish): ")
-    if movie_name.lower() == 'done':
+    Sonic_3 = input("Enter the movie name (or type 'done' to finish): ")
+    if Sonic_3.lower() == 'done':
         break
-    num_tickets = int(input(f"How many tickets for {movie_name}? "))
+    num_tickets = int(input(f"How many tickets for {Sonic_3}? "))
     total_tickets += num_tickets
 
 print(f"Total number of tickets desired: {total_tickets}")
